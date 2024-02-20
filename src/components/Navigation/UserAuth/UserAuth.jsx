8302600fe111ca065f59ac2cc106e6edc8064075
BizonNT <137486029+BizonNT@users.noreply.github.com>
2024-02-20 21:48:09 +0200

    reseterror

--- src/components/Navigation/UserAuth/UserAuth.jsx
+++ src/components/Navigation/UserAuth/UserAuth.jsx
@@ -1,19 +1,39 @@
 import { NavLink } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
-import Button from 'components/Button/Button';
+import Button from '../../Button/Button';
+import Message from '../../Message/Message';
+import { resetError } from '../../../redux/auth/auth-slice';
+import { selectError } from '../../../redux/auth/auth-selectors';
 
 import css from './user-auth.module.css';
 
 const UserAuth = () => {
+  const isError = useSelector(selectError);
+  let authError = isError;
+  const dispatch = useDispatch();
+
+  const handleResetError = () => {
+    authError = null;
+      dispatch(resetError());
+  };
 
   return (
     <div className={css.container}>
       <NavLink to="/register">
-        <Button title="Registration" type="submit" menu />
+        <Button
+          onClick={handleResetError}
+          title="Registration"
+          type="submit"
+          menu
+        />
       </NavLink>
       <NavLink to="/login">
-        <Button title="Login" type="submit" menu />
+        <Button onClick={handleResetError} title="Login" type="submit" menu />
       </NavLink>
+      {authError && (
+        <Message message={isError} red text=". Please, Try again..." />
+      )}
     </div>
   );
 };
--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -3,14 +3,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import Loader from 'components/Loader/Loader';
 
 import { signup } from '../../redux/auth/auth-operations';
-import { selectError, selectIsLoading } from '../../redux/auth/auth-selectors';
+import { selectIsLoading } from '../../redux/auth/auth-selectors';
 
 import RegisterForm from 'components/RegisterForm/RegisterForm';
-import Message from 'components/Message/Message';
 
 const RegisterPage = () => {
   const authLoading = useSelector(selectIsLoading);
-  const authError = useSelector(selectError);
 
   const dispatch = useDispatch();
 
@@ -21,9 +19,6 @@ const RegisterPage = () => {
   return (
     <>
       {authLoading && <Loader />}
-      {authError && (
-        <Message message={authError} red text=". Please, Try again..." />
-      )}
       {!authLoading && <RegisterForm onSubmit={handleSighUp} />}
     </>
   );
